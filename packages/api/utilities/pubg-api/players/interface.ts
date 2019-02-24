a58ca9a82953e14ea1571228d567e42ddb91832a
Leon Machens <leon.machens@googlemail.com>
2019-02-24 20:16:19 +0100

    fix player typings

--- packages/api/utilities/pubg-api/players/interface.ts
+++ packages/api/utilities/pubg-api/players/interface.ts
@@ -1,11 +1,38 @@
 type Players = Player[];
 
 interface Player {
-  type: 'player';
-  id: string;
-  attributes: any;
-  relationships: any;
-  links: any;
+  data: {
+    type: 'player';
+    id: string;
+    attributes: {
+      name: string;
+      createdAt: string;
+      updatedAt: string;
+      patchVersion: string;
+      stats: null;
+      titleId: string;
+      shardId: string;
+    };
+    relationships: {
+      matches: {
+        data: {
+          type: 'match';
+          id: string;
+        }[];
+      };
+      assets: {
+        data: [];
+      };
+    };
+    links: {
+      schema: '';
+      self: string;
+    };
+  };
+  links: {
+    self: string;
+  };
+  meta: {};
 }
 
 export default Players;
