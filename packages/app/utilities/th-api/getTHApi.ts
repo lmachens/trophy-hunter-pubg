fe74bf1809234077d2c42d59c38ccfa3c565ba91
Leon Machens <leon.machens@googlemail.com>
2019-03-16 15:21:54 +0100

    fix now config

--- packages/app/utilities/th-api/getTHApi.ts
+++ packages/app/utilities/th-api/getTHApi.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.TH_PUBG_API || 'https://pubg-api.th.gl',
+  baseURL: process.env.TH_PUBG_API,
   timeout: 5000,
   headers: {
     Accept: 'application/json',
