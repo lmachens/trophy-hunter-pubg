c09ef80aee765c4d81354105b93aa2eb0cd530df
Leon Machens <leon.machens@googlemail.com>
2019-04-07 14:45:23 +0200

    add hotkey

--- packages/app/components/GameListener/GameListener.tsx
+++ packages/app/components/GameListener/GameListener.tsx
@@ -66,6 +66,20 @@ const GameListener: FunctionComponent = () => {
 
   useEffect(() => {
     if (typeof overwolf !== 'undefined') {
+      overwolf.settings.registerHotKey('th_pubg', (hotkey: any) => {
+        if (hotkey.status == 'success') {
+          overwolf.windows.getCurrentWindow((result: any) => {
+            if (result.status == 'success') {
+              if (result.window.isVisible) {
+                overwolf.windows.hide(result.window.id, () => {});
+              } else {
+                overwolf.windows.restore(result.window.id, () => {});
+              }
+            }
+          });
+        }
+      });
+
       overwolf.games.onGameInfoUpdated.addListener(handleGameInfoUpdated);
       overwolf.games.getRunningGameInfo(handleRunningGameInfo);
 
