3a39f4050f57604276c1021a19ccb0c5a6f4b6e1
Leon Machens <leon.machens@googlemail.com>
2019-04-24 10:39:16 +0200

    add new trophies

--- packages/api/utilities/trophies/definitions/index.ts
+++ packages/api/utilities/trophies/definitions/index.ts
@@ -9,15 +9,21 @@ import zombie from './zombie';
 import collector from './collector';
 import champions from './champions';
 import panzerfaust from './panzerfaust';
+import assistent from './assistent';
+import fat from './fat';
+import serialKiller from './serialKiller';
 
 const trophies = {
   [addicted.name]: addicted,
+  [assistent.name]: assistent,
   [champions.name]: champions,
   [collector.name]: collector,
+  [fat.name]: fat,
   [healer.name]: healer,
   [panzerfaust.name]: panzerfaust,
   [roadKills.name]: roadKills,
   [runner.name]: runner,
+  [serialKiller.name]: serialKiller,
   [sniper.name]: sniper,
   [straightShots.name]: straightShots,
   [swimmer.name]: swimmer,
