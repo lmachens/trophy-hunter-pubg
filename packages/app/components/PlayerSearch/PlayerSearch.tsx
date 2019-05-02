167fc228a5fc506bb3f2f0a5f66f2394cf29fb1f
Leon Machens <leon.machens@googlemail.com>
2019-05-02 08:34:38 +0200

    add case-sensitive note

--- packages/app/components/PlayerSearch/PlayerSearch.tsx
+++ packages/app/components/PlayerSearch/PlayerSearch.tsx
@@ -1,6 +1,15 @@
 import React, { FunctionComponent, useState } from 'react';
 import { makeStyles } from '@material-ui/styles';
-import { IconButton, InputBase, MenuItem, Paper, Select, Snackbar, Theme } from '@material-ui/core';
+import {
+  IconButton,
+  InputBase,
+  MenuItem,
+  Paper,
+  Select,
+  Snackbar,
+  Theme,
+  Typography
+} from '@material-ui/core';
 import Search from '@material-ui/icons/Search';
 import getPlayer from 'utilities/th-api/player';
 import { useChangeAccount, useAccount } from 'contexts/account';
@@ -37,13 +46,23 @@ const useStyles = makeStyles((theme: Theme) => ({
 }));
 
 const platforms = ['Steam', 'PSN', 'XBOX', 'Kakao'];
-
+const formatError = (error: any) => {
+  if (!error) {
+    return '';
+  }
+  if (error.response && error.response.status) {
+    if (error.response.status === 404) {
+      return 'Can not find player';
+    }
+  }
+  return error.message;
+};
 const PlayerSearch: FunctionComponent<PlayerSearchProps> = ({ autoFocus, className }) => {
   const classes = useStyles();
   const [playerName, setPlayerName] = useState('');
   const [platform, setPlatform] = useState('Steam');
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<Error | null>(null);
+  const [error, setError] = useState<any>(null);
   const changeAccount = useChangeAccount();
   const account = useAccount();
 
@@ -72,7 +91,7 @@ const PlayerSearch: FunctionComponent<PlayerSearchProps> = ({ autoFocus, classNa
         setPlayerName('');
         Router.push(`/player?platform=${player.platform}&playerName=${player.name}`);
       })
-      .catch((error: Error) => {
+      .catch(error => {
         console.error(error);
         setError(error);
         setLoading(false);
@@ -130,7 +149,13 @@ const PlayerSearch: FunctionComponent<PlayerSearchProps> = ({ autoFocus, classNa
           'aria-describedby': 'message-id',
           className: classes.error
         }}
-        message={<span id="message-id">{error && error.message}</span>}
+        message={
+          <span id="message-id">
+            {formatError(error)}
+            <br />
+            <Typography variant="caption">Note: Player search is case-sensitive</Typography>
+          </span>
+        }
       />
     </form>
   );
